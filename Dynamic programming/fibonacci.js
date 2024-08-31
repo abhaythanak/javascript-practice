67db01031d5f77b24c1fb5a02197b9c618fc668c
abhaythanak <abhaythanak@gmail.com>
2024-08-31 11:31:38 +0530

    feat: add memoized Fibonacci function- Implemented a recursive Fibonacci function with memoization.- Cached results to improve performance.- Tested with n=70, outputting 190392490709135.

--- Dynamic programming/fibonacci.js
+++ Dynamic programming/fibonacci.js
@@ -1,3 +1,26 @@
-                                  
-                                  // fibonacci    
-                                              
+// fibonacci
+
+function Fib (n,rlt={}){
+    if (n in rlt) return rlt[n]; 
+    if (n<=2) return 1 ;
+    rlt[n] = Fib(n-1,rlt) + Fib(n-2,rlt)
+    return rlt[n]
+}
+
+console.log(Fib(70))
+
+function Fib(n) {
+    if (n <= 2) return 1;
+
+    let a = 1, b = 1;
+    
+    for (let i = 3; i <= n; i++) {
+        let temp = a + b;
+        a = b;
+        b = temp;
+    }
+    
+    return b;
+}
+
+console.log(Fib(80));
